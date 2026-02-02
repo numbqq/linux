5f7ac35d908164813f8a45ac3adb141572c739ae
Nick Xie <nick@khadas.com>
2026-02-02 14:56:29 +0800

    enable panfrost
    Signed-off-by: Nick Xie <nick@khadas.com>

--- drivers/gpu/drm/panfrost/panfrost_devfreq.c
+++ drivers/gpu/drm/panfrost/panfrost_devfreq.c
@@ -206,7 +206,7 @@ int panfrost_devfreq_init(struct panfrost_device *pfdev)
 	pfdevfreq->gov_data.downdifferential = 5;
 
 	devfreq = devm_devfreq_add_device(dev, &panfrost_devfreq_profile,
-					  DEVFREQ_GOV_SIMPLE_ONDEMAND,
+					  DEVFREQ_GOV_PERFORMANCE,
 					  &pfdevfreq->gov_data);
 	if (IS_ERR(devfreq)) {
 		DRM_DEV_ERROR(dev, "Couldn't initialize GPU devfreq\n");
