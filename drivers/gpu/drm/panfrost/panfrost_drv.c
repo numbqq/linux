f7311c684686125aec2b71ae4852a3b6b1132079
Nick Xie <nick@khadas.com>
2026-02-28 15:19:38 +0800

    gpu-panfrost-add-meson-s4-support
    Signed-off-by: Nick Xie <nick@khadas.com>

--- drivers/gpu/drm/panfrost/panfrost_drv.c
+++ drivers/gpu/drm/panfrost/panfrost_drv.c
@@ -1156,6 +1156,8 @@ static const struct of_device_id dt_match[] = {
 	  .data = &amlogic_data, },
 	{ .compatible = "amlogic,meson-g12a-mali",
 	  .data = &amlogic_data, },
+	{ .compatible = "amlogic,meson-s4-mali",
+	  .data = &amlogic_data, },
 	{ .compatible = "renesas,r9a09g047-mali", .data = &default_pm_rt_data },
 	{ .compatible = "arm,mali-t604", .data = &default_data, },
 	{ .compatible = "arm,mali-t624", .data = &default_data, },
